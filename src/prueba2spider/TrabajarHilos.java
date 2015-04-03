2a426ef08fcdfc9bf548ca9539e4e8fdb057dbe2
rafael <rafael@rafael-X551MA>
2015-04-02 20:00:26 -0600

    con clase hilos

---
+++ src/prueba2spider/TrabajarHilos.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba2spider;
+
+/**
+ *Clase que se utiliza para crear hilo
+ * @author rafael
+ */
+public class TrabajarHilos implements Runnable {
+
+    Thread t;
+    String nombre;
+    /**
+ *Metodo que se utiliza para crear hilo
+ * @author rafael
+ */
+    public TrabajarHilos (){
+        
+        t= new Thread(this,"hilo1");
+        t.start();
+       
+    }
+    /**
+ *Metodo que corre el hilo 
+ * @author rafael
+ */
+    public TrabajarHilos (String nombre){
+        this.nombre=nombre;
+        t= new Thread(this,"hilo1");
+        t.start();
+       
+    }
+    
+    
+    public void run() {
+        try{
+            for(int i=1;i<=5;i++){
+                
+                System.out.println(nombre+" "+i);
+                Thread.sleep(500);
+                
+            }
+            
+            
+        }catch(InterruptedException e){};
+
+    
+    }
+    
+}
